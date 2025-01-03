5a9ae35ba055d13d9011b6d7e4f10f82dc4b7a46
Axel Gil <73158076+AxelGil@users.noreply.github.com>
2025-01-03 23:18:43 +0100

    refactor: Simplify CapteurGlucose class by removing measurement methods

--- app/cgm_app/cgm.py
+++ app/cgm_app/cgm.py
@@ -1,14 +1,5 @@
 import random
 
 class CapteurGlucose:
-    def mesurer(self):
-        # Simulation d'une mesure aléatoire (à remplacer par une vraie mesure)
-        taux_glucose = random.randint(60, 180)
-        return taux_glucose
-
-    def transmettreDonnees(self, recepteur):
-        taux_glucose = self.mesurer()
-        recepteur.recevoirDonnees(taux_glucose)
-
-    def configurer(self, unite_mesure='mg/dL'):
-        self.unite_mesure = unite_mesure    
+    def __init__(self):
+     pass
